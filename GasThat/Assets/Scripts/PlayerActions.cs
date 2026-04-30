f078211e06ea68d8d618afee3219191237223d15
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-04-30 10:32:15 -0400

    Adding a gun that shoots gas

--- GasThat/Assets/Scripts/PlayerActions.cs
+++ GasThat/Assets/Scripts/PlayerActions.cs
@@ -5,6 +5,8 @@ public class PlayerActions : MonoBehaviour
 {
     [SerializeField] private Transform look;
     [SerializeField] private LayerMask hitLayer;
+
+    [SerializeField] private Weapon inHandWeapon;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,20 +22,28 @@ public class PlayerActions : MonoBehaviour
 
     public void OnShoot(InputAction.CallbackContext context)
     {
-        if (!context.performed) return;
-        Debug.Log("Shoot!");
-        if (Physics.Raycast(look.position, look.TransformDirection(Vector3.forward) * 100, out var hit, Mathf.Infinity,
-                hitLayer))
+        if (context.performed)
         {
-            Debug.Log("Hit!");
-            if (hit.collider.gameObject.TryGetComponent<Enemy>(out var enemy))
+            Debug.Log("Shoot!");
+            inHandWeapon.Shoot();
+            if (Physics.Raycast(look.position, look.TransformDirection(Vector3.forward) * 100, out var hit,
+                    Mathf.Infinity,
+                    hitLayer))
             {
-                enemy.GetHit();
+                Debug.Log("Hit!");
+                if (hit.collider.gameObject.TryGetComponent<Enemy>(out var enemy))
+                {
+                    enemy.GetHit();
+                }
+            }
+            else
+            {
+                Debug.Log("No Hit!");
             }
         }
-        else
+        else if (context.canceled)
         {
-            Debug.Log("No Hit!");
+            inHandWeapon.Release();
         }
     }
 }
---
+++ GasThat/Assets/Scripts/Weapon.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public enum ShootType
+{
+    Single,
+    Burst,
+    Hold
+}
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private ShootType shootType;
+    [SerializeField] private GameObject particle;
+    [SerializeField] private Transform barrelTip;
+    [SerializeField] private float destroyTimer = 5.0f;
+
+    private GameObject _currentParticle;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentParticle != null)
+        {
+            _currentParticle.transform.position = barrelTip.position;
+            _currentParticle.transform.rotation = barrelTip.rotation;
+        }
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("shoot my gun");
+        _currentParticle = Instantiate(particle, barrelTip.position, barrelTip.rotation);
+    }
+
+    public void Release()
+    {
+        if (shootType == ShootType.Hold)
+        {
+            Destroy(_currentParticle, destroyTimer);
+            _currentParticle.GetComponent<ParticleSystem>().Stop();
+            _currentParticle = null;
+        }
+    }
+}
